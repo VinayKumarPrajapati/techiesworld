1f8309fd633621d1b9026e5b1228e0b94f8367bb
vinaykumarprajapati <sagaipoint@gmail.com>
2023-02-25 22:22:42 +0530

    small changes

--- client/src/auth/Register.js
+++ client/src/auth/Register.js
@@ -1,8 +1,19 @@
 import { useState } from "react";
 const Register = () => {
-  const [name, setName] = useState("");
+  const [contactNumber, setContactNumber] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const handleSubmit = () => {
+    alert("checking code"); }
+  const registerForm = () => ( <form onSubmit={handleSubmit}> <div> <label>Your Contact Number</label>
+  <input
+  type="text"
+  placeholder="Enter Contact Number"
+  value={contactNumber}
+  onChange={(e) => setName(e.target.value)}
+  />
+  </div>
+  </form>   );
   return (
     <>
       <div>
