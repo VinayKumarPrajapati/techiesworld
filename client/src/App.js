b7baf6e75f444c5c1cc9bbe6e70e4e26d802c10f
vinaykumarprajapati <sagaipoint@gmail.com>
2023-02-25 14:03:14 +0530

    gitignore

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from './bookingtechies/Home';
+import Login from './auth/Login';
+import Register from './auth/Register';
+import TopNav from "./components/TopNav";
 
 function App() {
   return (
-    <div className="App">
-   a
-    </div>
+   <Router>
+    {TopNav}
+     <Routes>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+     </Routes>
+   </Router>
   );
 }
 
--- client/src/bookingtechies/Home.js
+++ client/src/bookingtechies/Home.js
@@ -1,4 +1,10 @@
+import { userSelector, useSelector, useStore } from "react-redux";
 const Home = () => {
-    return <div className="container-fluid h1 p-5 text-center">Home Page</div>;
+const state = useSelector((state) => state);
+return (
+<div className="container-fluid h1 p-5 text-center">
+Home Page {JSON.stringify(state)}
+</div>
+);
 };
 export default Home;
